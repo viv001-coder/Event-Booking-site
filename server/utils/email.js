4aa77c4cf262dfafebcd77e2c366811437ec8255
viv001-coder <rviv66@gmail.com>
2026-06-13 00:55:11 +0530

    Fix Gmail SMTP config

--- server/utils/email.js
+++ server/utils/email.js
@@ -5,7 +5,9 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
