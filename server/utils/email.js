6b72bbf46ef06b4b5fbedb4eec7a2d7c9a41226c
viv001-coder <rviv66@gmail.com>
2026-06-13 01:08:37 +0530

    Switch Gmail SMTP to port 587

--- server/utils/email.js
+++ server/utils/email.js
@@ -6,11 +6,14 @@ dotenv.config();
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
+    },
+    tls: {
+        rejectUnauthorized: false
     }
 });
 
